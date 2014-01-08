35eefa3a6ba1043b84c15ab181e11a0c3f2fc068
Sebastian Wojtczak <wojtczaksebastian@gmail.com>
2014-01-08 12:46:33 +0100

    Items added to order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -69,6 +69,7 @@ class OrdersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
-      params.require(:order).permit(:payer_name, :address, :tax_id, :country, :email)
+      params.require(:order).permit(:payer_name, :address, :tax_id, :country,
+        :email, item_ids: [])
     end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ActiveRecord::Base
+  has_many :line_items
+  has_many :items, through: :line_items
 end
