46f3732d6732d5407e7411b90f79dff082447a2f
Sebastian Wojtczak <wojtczaksebastian@gmail.com>
2014-01-08 11:40:06 +0100

    Added Item model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 GeneratePdf::Application.routes.draw do
+  resources :items
+
   resources :orders
 
   root :to => "home#index"
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140107093306) do
+ActiveRecord::Schema.define(version: 20140108093047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "orders", force: true do |t|
     t.string   "payer_name"
